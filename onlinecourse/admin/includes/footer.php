becb8c71fbaf6f97b48e9a30c5313df7183ab238
gowtham2k2 <93262392+gowtham2k2@users.noreply.github.com>
2021-12-21 20:32:37 +0530

    Add files via upload
    first commit

---
+++ onlinecourse/admin/includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2021 Online Course Registration | By : Gowtham Gandhi,Harshvardhan,Jeevananthan
+                </div>
+
+            </div>
+        </div>
+    </footer>
